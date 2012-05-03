64a81a116ed65fa891441375f2ee55e1a9f8ef77
Joshua Pekera <joshua@smagency.com>
2012-05-03 03:50:04 -0500

    Updated Theme
    Updated theme to support Pryro 2.0.3 and Bootstrap 2.0

--- theme.php
+++ theme.php
@@ -1,13 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Theme_Bootstrap extends Theme {
+class Theme_TwitterBootstrap extends Theme {
 
-    public $name = 'Bootstrap';
-    public $author = 'Tim Reynolds';
-    public $author_website = 'http://www.tomothyreynolds.co.uk/';
-    public $website = 'http://www.pyrocms.com/';
-    public $description = 'Bootstap template.';
-    public $version = '1.0b';
+    public $name = 'Twitter Bootstrap';
+    public $author = 'Joshua Pekera';
+    public $author_website = 'http://joshuapekera.com/';
+    public $website = 'http://twitter.github.com/bootstrap';
+    public $description = '12-column, responsive theme using Twitter Bootstrap 2.';
+    public $version = '2.0';
     public $options = array('show_breadcrumbs' => array(
                      	         'title' => 'Show Breadcrumbs',
 							'description'   => 'Would you like to display breadcrumbs?',
